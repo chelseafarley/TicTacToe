185f0916ecc42ca466175fb196d712f106ebf79a
Chelsea Farley <chelseafarley91@gmail.com>
2021-11-20 10:01:25 +1300

    Day 20 - Tic Tac Toe watch app

---
+++ TicTacToeWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  TicTacToeWatch WatchKit Extension
+//
+//  Created by chelsea on 20/11/21.
+//  Copyright © 2021 tripwiretech. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
